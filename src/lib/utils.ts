d4c01e293a698dd63913e892b5f6289c3940a06c
Fallen-dev <rishav.m.19.02@gmail.com>
2023-11-24 15:23:14 +0530

    update blogs function with more features

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,21 @@
 import type { MarkdownHeading } from 'astro'
+import type {CollectionEntry} from 'astro:content'
 import { getCollection } from 'astro:content'
 
-const _blogs = await getCollection('blog', (post) =>
-  process.env.NODE_ENV == 'production' ? post.data.date : post)
+export async function getBlogs(fn?: (p: CollectionEntry<'blog'>) => boolean) {
+  const data = await getCollection('blog', fn)
+  
+  const blogs = data.map(
+    post =>
+      process.env.NODE_ENV == 'production' ? post.data.date : post
+  ) as CollectionEntry<'blog'>[]
 
-// @ts-expect-error
-export const blogs = _blogs.sort((a, b) => {
-  if (!a.data.date || !b.data.date) return
-
-  return b.data.date.valueOf() - a.data.date.valueOf()
-})
+  //@ts-ignore
+  return blogs.sort((a, b) => {
+    if (!a.data.date || !b.data.date) return
+    return b.data.date.valueOf() - a.data.date.valueOf()
+  })
+}
 
 export function createTOC(headings: MarkdownHeading[]) {
   const toc = [] as any[]
