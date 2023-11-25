5e42b3e68af17d843089e7d791e51fac3fe009e5
Fallen-dev <rishav.m.19.02@gmail.com>
2023-11-25 22:13:06 +0530

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import readingTime from "./src/readingTime";
-import svelte from "@astrojs/svelte";
+// import svelte from "@astrojs/svelte";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), svelte()],
-  site: process.env.NODE_ENV == 'production' ? 'https://fallenblogs.vercel.app' : 'http://localhost:4321',
+  integrations: [mdx()],
+  // site: process.env.NODE_ENV == 'production' ? 'https://fallenblogs.vercel.app' : 'http://localhost:4321',
   markdown: {
     remarkPlugins: [readingTime],
     shikiConfig: {
