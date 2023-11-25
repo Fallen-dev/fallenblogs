fbf42b0af5515bb22bdbb78b6c7757bdd91d9ee1
Fallen-dev <rishav.m.19.02@gmail.com>
2023-11-25 22:27:50 +0530

    test #4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,25 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-// import readingTime from "./src/readingTime";
-// import svelte from "@astrojs/svelte";
+import readingTime from "./src/readingTime";
+import svelte from "@astrojs/svelte";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  // site: process.env.NODE_ENV == 'production' ? 'https://fallenblogs.vercel.app' : 'http://localhost:4321',
+  site: process.env.NODE_ENV == 'production' ? 'https://fallenblogs.vercel.app' : 'http://localhost:4321',
   markdown: {
-    // remarkPlugins: [readingTime],
-    // shikiConfig: {
-    //   theme: 'material-theme-darker'
-    // },
-    // image: {
-    //   domains: ['astro.build'],
-    //   remotePatterns: [{
-    //     protocol: 'https'
-    //   }]
-    // }
+    remarkPlugins: [readingTime],
+    shikiConfig: {
+      theme: 'material-theme-darker'
+    },
+    image: {
+      domains: ['astro.build'],
+      remotePatterns: [{
+        protocol: 'https'
+      }]
+    }
   },
   output: "server",
   adapter: vercel()
