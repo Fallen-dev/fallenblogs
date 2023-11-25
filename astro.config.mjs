6dc887d02169acbacc0b62340b25aa629757e664
Fallen-dev <rishav.m.19.02@gmail.com>
2023-11-25 22:36:21 +0530

    edge function

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 import mdx from "@astrojs/mdx";
 import readingTime from "./src/readingTime.mjs";
 
