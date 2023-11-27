20d7eec6e145f346a5ea2af78b74a358a1d14427
Fallen-dev <rishav.m.19.02@gmail.com>
2023-11-27 18:07:21 +0530

    tets #1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-import readingTime from "./src/readingTime.js";
+import readingTime from "./src/readingTime.mjs";
 import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
@@ -8,7 +8,7 @@ export default defineConfig({
   adapter: vercel(),
   output: "server",
   integrations: [mdx()],
-  site: process.env.NODE_ENV == 'production' ? 'https://fallenblogs.vercel.app' : 'http://localhost:4321',
+  //site: process.env.NODE_ENV == 'production' ? 'https://fallenblogs.vercel.app' : 'http://localhost:4321',
   markdown: {
     remarkPlugins: [readingTime],
     shikiConfig: {
