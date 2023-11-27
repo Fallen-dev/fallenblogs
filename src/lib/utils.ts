4cec3cf8f6a78c0e114cdef3181384913de85372
Fallen-dev <rishav.m.19.02@gmail.com>
2023-11-27 19:43:00 +0530

    OMG

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,4 @@
 import type { MarkdownHeading } from 'astro'
-import type {CollectionEntry} from 'astro:content'
-import { getCollection } from 'astro:content'
-
-export async function getBlogs(fn?: (p: CollectionEntry<'blog'>) => boolean | CollectionEntry<'blog'>) {
-  const data = await getCollection('blog', function (post) {
-    return process.env.NODE_ENV?.toLowerCase() == 'production'
-    ? fn || post.data.date != undefined
-    : fn || post
-  })
-
-  return data.sort((fst, snd) => {
-    if (!fst.data.date || !snd.data.date) return 0
-
-    return snd.data.date.valueOf() - fst.data.date.valueOf()
-  })
-}
 
 export function createTOC(headings: MarkdownHeading[]) {
   const toc = [] as any[]
