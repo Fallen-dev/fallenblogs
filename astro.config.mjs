fa76971231770da7e955f17cb136807dfde0f8c2
Fallen-dev <rishav.m.19.02@gmail.com>
2023-11-27 18:18:08 +0530

    fully revert back to static mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import readingTime from "./src/readingTime.mjs";
-import vercel from '@astrojs/vercel/serverless'
+// import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
-  output: "server",
+  // adapter: vercel(),
+  // output: "server",
   integrations: [mdx()],
   //site: process.env.NODE_ENV == 'production' ? 'https://fallenblogs.vercel.app' : 'http://localhost:4321',
   markdown: {
